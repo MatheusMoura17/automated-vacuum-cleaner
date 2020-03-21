cfcfcd4ef119a3cff35724a42508276ec08451c6
Matheus Moura <matheus.moura13@gmail.com>
2020-03-21 15:57:37 -0300

    Consome dados no script principal

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
-from pyfirmata import Arduino, util, SERVO
 import time
-import math
 
-board = Arduino("COM4")
+from dataio import Audio
+from dataio import Robot
 
-board.digital[3].mode = SERVO
+stop = False
 
-servo_pin = board.get_pin('d:3:s')
 
-servo_pin.write(170)
-time.sleep(1000)
-exit()
+# Uma nova frase foi reconhecida
+def speech_recognized(phrase):
+    print(phrase)
+    entry = phrase.lower()
+    if entry == "isso é um assalto":
+        Robot.hands_up()
+        Audio.say("por favor não me machuque")
+        return
+
+    if entry == "de pé":
+        Robot.view_top()
+        return
+
+    if entry == "desligar":
+        stop = True
+        return
+
+    Audio.say("Não entendi o que você falou")
+
+
+audioInputSensor = Audio.Input()
+audioInputSensor.start(speech_recognized)
+
+while not stop:
+    time.sleep(1000)
