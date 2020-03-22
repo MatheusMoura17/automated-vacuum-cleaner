5fe206d85d0e6543d962f3f7a4735ab97a1cc760
Matheus Moura <matheus.moura13@gmail.com>
2020-03-22 11:33:34 -0300

    Troca aspas duplas por simples

--- dataio/Audio.py
+++ dataio/Audio.py
@@ -7,10 +7,10 @@ from gtts import gTTS
 import os
 from playsound import playsound
 
-if not os.path.exists("audios/recorded"):
-    os.makedirs("audios/recorded")
-if not os.path.exists("audios/auto"):
-    os.makedirs("audios/auto")
+if not os.path.exists('audios/recorded'):
+    os.makedirs('audios/recorded')
+if not os.path.exists('audios/auto'):
+    os.makedirs('audios/auto')
 
 
 class Input:
@@ -20,21 +20,21 @@ class Input:
     # Algum texto foi reconhecido
     def _result(self, recognizer, audio):
         try:
-            phrase = recognizer.recognize_google(audio, language="pt-BR")
-            raw_phrase = phrase.replace(" ", "_").lower()
+            phrase = recognizer.recognize_google(audio, language='pt-BR')
+            raw_phrase = phrase.replace(' ', '_').lower()
             raw_phrase = unidecode.unidecode(raw_phrase)
 
             # Salva o audio caso nao exista
-            if not os.path.exists(f"audios/recorded/{raw_phrase}.wav"):
-                with open(f'audios/recorded/{raw_phrase}.wav', "wb") as f:
+            if not os.path.exists(f'audios/recorded/{raw_phrase}.wav'):
+                with open(f'audios/recorded/{raw_phrase}.wav', 'wb') as f:
                     f.write(audio.get_wav_data())
                     fx(f'audios/recorded/{raw_phrase}.wav', f'audios/recorded/{raw_phrase}__effect.wav')
 
             self.callback(phrase)
         except sr.UnknownValueError:
-            print("Google Speech Recognition could not understand audio")
+            print('Google Speech Recognition could not understand audio')
         except sr.RequestError as e:
-            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+            print('Could not request results from Google Speech Recognition service; {0}'.format(e))
 
     # Inicia a escuta de audio
     def start(self, callback):
@@ -44,7 +44,7 @@ class Input:
         with mic as source:
             rec.adjust_for_ambient_noise(source)
         rec.listen_in_background(mic, self._result, phrase_time_limit=3)
-        print("AudioInput Start")
+        print('AudioInput Start')
 
 
 fx = (AudioEffectsChain()
@@ -57,7 +57,7 @@ fx = (AudioEffectsChain()
 
 
 def say(text):
-    raw_text = text.replace(" ", "_").lower()
+    raw_text = text.replace(' ', '_').lower()
     raw_text = unidecode.unidecode(raw_text)
 
     # Se existir uma midia gravada por usuario, executamos
@@ -71,7 +71,7 @@ def say(text):
 
         tts.save(f'audios/auto/{raw_text}.mp3')
         wav = AudioSegment.from_mp3(f'audios/auto/{raw_text}.mp3')
-        wav.export(f'audios/auto/{raw_text}.wav', format="wav")
+        wav.export(f'audios/auto/{raw_text}.wav', format='wav')
         fx(f'audios/auto/{raw_text}.wav', f'audios/auto/{raw_text}__effect.wav')
 
     # Executa o arquivo
